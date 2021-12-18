c232f9c392ae0a42bf7e186f4730bbafe6c98cb2
@SoulOG <285963@supinfo.com>
2021-12-17 19:18:58 -0500

    commit

--- 3-Data_Structure/2-reversing_an_array.js
+++ 3-Data_Structure/2-reversing_an_array.js
@@ -1,5 +1,8 @@
-/*Arrays have a reverse method that changes the array by inverting the order in which its elements appear. For this exercise, write two functions, reverseArray and reverseArrayInPlace. The first, reverseArray, taies an array as argument and produces a new array that has the same elements in the inverse order. 
-The second, reverseArrayInPlace, does what the reverse method does: it modifies the array given as argument by reversing its elements. Neither may use the standard reverse method. */
+/*Arrays have a reverse method that changes the array by inverting the order in which its elements appear.
+For this exercise, write two functions, reverseArray and reverseArrayInPlace.
+The first, reverseArray, taies an array as argument and produces a new array that has the same elements in the inverse order.
+The second, reverseArrayInPlace, does what the reverse method does: it modifies the array given as argument by reversing its elements.
+Neither may use the standard reverse method. */
 
 let reverseArray = (tableau) => {
     revArray = [];
